9949afdc15c1194e1c85737811aef94e056b6b24
Marie Bittiehn <s0563863@htw-berlin.de>
2019-11-01 14:15:14 +0100

    initial commit

---
+++ LeafAlone/src/main/java/group11/leafalone/LeafaloneApplication.java
@@ -0,0 +1,13 @@
+package group11.leafalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafaloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafaloneApplication.class, args);
+	}
+
+}
