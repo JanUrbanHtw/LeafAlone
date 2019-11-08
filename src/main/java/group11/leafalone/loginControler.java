5131944d0b3e24e024902cb08ea40730adc12567
Jan <Jan@JANPC>
2019-11-08 15:34:55 +0100

    created login functionality

---
+++ src/main/java/group11/leafalone/loginControler.java
@@ -0,0 +1,16 @@
+package group11.leafalone;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class loginControler {
+
+    @RequestMapping("/login")
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login.html");
+        return modelAndView;
+    }
+}
