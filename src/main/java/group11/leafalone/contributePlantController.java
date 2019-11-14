52ed93f5611022a8fa8a286ab8e9a41bd7063729
cobock <s0563861@htw-berlin.de>
2019-11-14 23:00:48 +0100

    slight changes to file upload

--- src/main/java/group11/leafalone/contributePlantController.java
+++ src/main/java/group11/leafalone/contributePlantController.java
@@ -3,6 +3,11 @@ package group11.leafalone;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import javax.servlet.http.HttpSession;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 
 @Controller
 public class contributePlantController {
@@ -15,5 +20,26 @@ public class contributePlantController {
     @PostMapping("/contributePlant")
     public String contributePlantSubmit(@ModelAttribute PlantDescription plantdescription) {
         return "aboutUs";
+    }
+
+    @PostMapping("/savefile")
+    public String upload(@RequestParam CommonsMultipartFile file, HttpSession session){
+       String path=session.getServletContext().getRealPath("/");
+       String filename=file.getOriginalFilename();
+//
+//        System.out.println(path+" "+filename);
+        try{
+            byte barr[]=file.getBytes();
+
+            BufferedOutputStream bout=new BufferedOutputStream(
+                    new FileOutputStream(path+"/"+filename));
+            bout.write(barr);
+            bout.flush();
+            bout.close();
+
+        }catch(Exception e){System.out.println(e);}
+//        return new ModelAndView("upload-success","filename",path+"/"+filename);
+
+        return "contributePlant";
     }
 }
