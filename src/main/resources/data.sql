ca6506c7237aa927a059e9a7e5d1a21ca35ded60
JanUrban <urbanj@gmx.de>
2020-01-06 22:09:54 +0100

    Set ddl-auto to update
    To keep entries in the database after restarts
    
    See: #25

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,13 @@
 insert into leaf_alone_user (id, username, password, email, role)
-values (-1, 'user', '$2a$10$WrOoBjGqVDd4cCBxmpeMBO2zYVEeT0ixssruKpITP9ViF4aBDDqk2', 'leafalone@mail.de', 'ROLE_USER');
+values (-1, 'user', '$2a$10$WrOoBjGqVDd4cCBxmpeMBO2zYVEeT0ixssruKpITP9ViF4aBDDqk2', 'leafalone@mail.de', 'ROLE_USER')
+on conflict do nothing;
 
 insert into leaf_alone_user (id, username, password, email, role)
 values (-2, 'contributor', '$2a$10$WTnBAWaow./4WdtQRtYpBOjQ5dho3r4njRyM2nswWABcIr.BBXiCy',
-        'melonlord@posteo.de', 'ROLE_CONTRIBUTOR');
+        'melonlord@posteo.de', 'ROLE_CONTRIBUTOR')
+on conflict do nothing;
+
+
 
 insert into plant_care (id, colloquial, scientific, sun_situation, water_cycle,
                         water_amount, soil_advice, description, contributor_id)
@@ -11,7 +15,9 @@ values (-1, 'Jade Plant', 'Crassula Ovata', 0,
         1, 20,
         'soil specifically for succulents would be great, but anything works really',
         'A nice beginner plant. Very forgiving and somewhat resistant to damage of any kind',
-        -2);
+        -2)
+on conflict do nothing;
+
 
 insert into plant_care (id, colloquial, scientific, sun_situation, water_cycle,
                         water_amount, soil_advice, description, contributor_id)
@@ -19,7 +25,9 @@ values (-2, 'Bunny-Ears Cactus', 'Opuntia Microdasys', 0,
         1, 15,
         'succulent- or cactus-specific soil recommended',
         'A classy cactus type. But beware: cacti are not as resistant to bad care as often believed!',
-        -2);
+        -2)
+on conflict do nothing;
+
 
 
 -- insert into user_plant (name, plantcare_id, sun, acquisition, watered, next_watering, notes, user_id)
